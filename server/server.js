8eac49bcdd7dfa0bfbd9cd753128642994c15166
Gilles Bouthenot <misc@atomas.com>
2018-10-25 15:25:17 +0200

    server: cors

--- server/server.js
+++ server/server.js
@@ -5,26 +5,37 @@ const fastify = require('fastify')({
   logger: true
 })
 
+// main route
 fastify.get('/kind:kind/vol:vol/book:book/puzzle:puzzle', (request, reply) => {
   const rp = request.params
 
   const SQLITE = require('better-sqlite3')
   const db = new SQLITE('../leech/bridges.db')
-  const stmt = db.prepare('SELECT jsondata FROM bridges WHERE kind=? AND vol=? AND book=? AND number=?')
-  let ret = stmt.get(rp.kind, rp.vol, rp.book, rp.puzzle)
-  db.close()
+  let ret
+  try {
+    const stmt = db.prepare('SELECT jsondata FROM bridges WHERE kind=? AND vol=? AND book=? AND number=?')
+    ret = stmt.get(rp.kind, rp.vol, rp.book, rp.puzzle)
+  } catch (e) {
+    db.close()
+  }
 
   if (ret) {
     ret = JSON.parse(ret.jsondata)
+    reply.header('Access-Control-Allow-Origin', '*')
     reply.send(ret)
   } else {
     reply.status(404).send('Puzzle not found')
   }
 })
 
-// Declare a route
+// Index route
 fastify.get('/', (request, reply) => {
   reply.send({ hello: 'world' })
+})
+
+// "Not Found" route
+fastify.setNotFoundHandler((request, reply) => {
+  reply.code(404).type('text/html').send('Not Found')
 })
 
 // Run the server!
